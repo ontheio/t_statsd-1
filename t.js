b601fde523d552b4b30e1ae33ca4874ff119fca5
golotyuk <golotyuk@gmail.com>
2014-11-24 23:12:10 +0200

    Base64 encoding added to allow special symbols []"

--- t.js
+++ t.js
@@ -15,12 +15,16 @@ function TBackend(startupTime, config, emitter){
 
 TBackend.prototype.flush = function(timestamp, metrics) {
   var data = '';
-  for ( var m in metrics.counters ) if ( metrics.counters[m] ) data += '?k=' + api_id + ':' + m + '&v=' + metrics.counters[m] + '&s=' + md5(api_id + ':' + m + api_key) + " ";
-  for ( var m in metrics.gauges ) data += '?k=' + api_id + ':' + m + '&v==' + metrics.gauges[m] + '&s=' + md5(api_id + ':' + m + api_key) + " ";
+  for ( var m in metrics.counters ) if ( metrics.counters[m] ) data += '?k=' + api_id + ':' + resolve_metric(m) + '&v=' + metrics.counters[m] + '&s=' + md5(api_id + ':' + resolve_metric(m) + api_key) + " ";
+  for ( var m in metrics.gauges ) data += '?k=' + api_id + ':' + resolve_metric(m) + '&v==' + metrics.gauges[m] + '&s=' + md5(api_id + ':' + resolve_metric(m) + api_key) + " ";
   if ( !data.length ) return;
 
   send_t_data(data, 0);
 };
+
+function resolve_metric(m) {
+	return new Buffer(m, 'base64').toString('ascii');
+}
 
 function send_t_data(data, tries) {
   if ( tries >= 5 ) throw 'Failed to send data to t';
